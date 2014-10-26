f080e5a110640cc22caaf1fc661047038cbc138d
Liam Boone <liamboone@gmail.com>
2014-10-25 23:19:29 -0500

    Added in /<username> route

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ from elotron_backend import *
 
 app = flask.Flask(__name__)
 
+@app.route('/<uname>')
+def user(uname=''):
+    users = {u:get_display_name(u) for u in get_all_users()}
+    matches = [match for match in get_matches()
+               if (match['participants'][0][0] == uname or
+                   match['participants'][1][0] == uname)]
+    return flask.render_template('user.html', matches=matches,
+                                 uname=uname, users=users)
+
+
 @app.route('/')
 def index():
-    return "\n".join(map(get_display_name, get_all_users()))
     return flask.render_template('index.html')
 
 
